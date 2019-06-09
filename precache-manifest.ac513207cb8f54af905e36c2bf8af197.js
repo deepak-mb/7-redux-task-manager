993d4e1ac269efce9a1986cca3e74fac617cd0df
Deepak <deepak25mb@gmail.com>
2019-06-09 10:53:00 +0530

    Updates

---
+++ precache-manifest.ac513207cb8f54af905e36c2bf8af197.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b3f0fa523ffc0a9bc2f19568d8735ff",
+    "url": "/7-redux-task-manager/index.html"
+  },
+  {
+    "revision": "13401cf1b411601bc47a",
+    "url": "/7-redux-task-manager/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "bdc82097be88cc636115",
+    "url": "/7-redux-task-manager/static/js/2.f9259694.chunk.js"
+  },
+  {
+    "revision": "13401cf1b411601bc47a",
+    "url": "/7-redux-task-manager/static/js/main.74ad8d4c.chunk.js"
+  },
+  {
+    "revision": "22c8f0b5d6fdb97ff6c8",
+    "url": "/7-redux-task-manager/static/js/runtime~main.7adda118.js"
+  }
+]);
